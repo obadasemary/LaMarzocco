21b028fe9031ccfecf280d64567adf02c6285ed4
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-07-01 12:42:32 +0300

    Initial Commit

---
+++ LaMarzocco/LaMarzoccoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaMarzoccoApp.swift
+//  LaMarzocco
+//
+//  Created by Abdelrahman Mohamed on 1.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct LaMarzoccoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
