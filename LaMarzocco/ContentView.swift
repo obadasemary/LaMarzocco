a98626d37a3e7789cb14dcd1d36e39de91dd0d97
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-07-01 13:04:50 +0300

    feat: Expandable List View with Inset Grouped Style
    We introduced to you two new features for SwiftUI List view in this tutorial. As you can see in the demo, it is very easy to build an outline view or expandable list view.
    
    SwiftUI

--- LaMarzocco/ContentView.swift
+++ LaMarzocco/ContentView.swift
@@ -9,12 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!").padding()
+        List(sampleMenuItems, children: \.subMenuItems) { item in
+
+            HStack {
+
+                Image(item.image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 50, height: 50)
+
+                Text(item.name)
+                    .font(.system(.title3, design: .rounded))
+                    .bold()
+            }
+        }
+        .listStyle(InsetGroupedListStyle())
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 11 Pro Max")
     }
 }
